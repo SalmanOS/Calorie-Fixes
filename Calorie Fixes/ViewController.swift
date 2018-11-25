9cd4877691e6af302c0315949f522fcdc7788b5b
SalmanOS <salmanos@SalmanOSs-MacBook-Pro.local>
2018-11-25 21:22:16 +0600

    File Created

---
+++ Calorie Fixes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calorie Fixes
+//
+//  Created by SalmanOS on 25/11/18.
+//  Copyright © 2018 SalmanOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
